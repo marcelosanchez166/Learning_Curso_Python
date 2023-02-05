35e02e04cbc13aa9c6bc38bd52c74cffcef9d67e
marcelosanchez166 <marcelosanchez166@gmail.com>
2023-02-04 22:15:10 -0600

    Aprendiendo sobre Entornos virtuales

--- cumple.py
+++ cumple.py
@@ -1,4 +1,4 @@
-#Escribir un programa que pregunte al usuario su edad y muestre por pantalla todos los años que ha cumplido (desde 1 hasta su edad).
-edad=int(input("Ingrese su edad "))
+# Escribir un programa que pregunte al usuario su edad y muestre por pantalla todos los años que ha cumplido (desde 1 hasta su edad).
+edad = int(input("Ingrese su edad "))
 for i in range(edad):
-          print("Has cumplido ", i+1, "años")
+    print("Has cumplido ", i+1, "años")
