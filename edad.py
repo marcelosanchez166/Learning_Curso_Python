bfc10e2cfaa72a5ea5cbecc1ba5b5b8e758308d5
marcelosanchez166 <marcelosanchez166@gmail.com>
2023-02-24 10:01:17 -0600

    Cambios en archivos

--- edad.py
+++ edad.py
@@ -1,6 +1,6 @@
-#Escribir un programa que pregunte al usuario su edad y muestre por pantalla si es mayor de edad o no.
-edad=int(input("Ingrese su edad "))
-if edad >=18:
-          print("Usted es mayor de edad bienvenido ")
+# Escribir un programa que pregunte al usuario su edad y muestre por pantalla si es mayor de edad o no.
+edad = int(input("Ingrese su edad "))
+if edad >= 18:
+    print("Usted es mayor de edad bienvenido ")
 else:
-          print("Usted no es mayor de edad no tiene permitido el acceso ")
+    print("Usted no es mayor de edad no tiene permitido el acceso ")
---
+++ piedrapapeltijeraslagartospok.py
@@ -0,0 +1,18 @@
+def rock_paper_scissor_lizard_spock(games):
+    rules = {""}
+    player_one = 0
+    player_two = 0
+     for game in games:
+        player_one_game = game[0]
+        player_two_game - gane(a)
+         if player_one_game a player_two_game:
+             if player_two_game in rules[player_one_game]:
+                player_one += 1
+            else:
+                player_two += 1
+     return "Tie" if player_one player_two else "Player 1" if player_one > player_two el
+print(rock_paper_scissor_lizard_spock((*
+print(rock_paper_scissor_lizard_spock([())))
+print(rock_paper_scissor_lizard_spock(( "xe"(٩")]))
+print(rock_paper_scissor_lizard_spock([("", "7"), ("4", "7"), ("7" ("7" 4 (()))
+print( (rock_paper_scissor_lizard_spock([("y" 7"), "x""), ("7", "7"), ("X"")]))
